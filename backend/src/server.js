c8a4a12582b15c65fedf813ca910abce0593eeda
DuyVo2005 <vlkd1001@gmail.com>
2026-05-12 20:37:05 +0700

    update code be đồng bộ cấu trúc với nhóm, up code fe edit Profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,10 @@
 import express from "express"
-import bodyParser from "body-parser"//Để lấy tham số từ client vd: /user?id=7
-import viewEngine from "./config/viewEngine"
-import initWebRoutes from "./route/web"
 import connectDB from './config/configdb'
 require("dotenv").config();//Để chạy lệnh process.env.PORT
 
 let app=express()
 
 //config app
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-viewEngine(app);
-initWebRoutes(app);
 connectDB();
 
 let port=process.env.PORT || 6969;
